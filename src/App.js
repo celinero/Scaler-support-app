5522e14f6b29e0ec03bab03aa232e6ef586d8e16
celine.roure <celine.roure.pro@gmail.com>
2021-12-12 11:04:10 +1100

    adds useState and useEffect to display tickets, later on, on the homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {getTickets} from './services/ticketServices'
 
 const App = () => {
+  const [tickets, setTickets] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    getTickets() 
+      .then(tickets => {
+        console.log(tickets)
+        setTickets(tickets)}
+        )
+      .catch(error => console.log(error))
+      .finally(()=> setLoading(false))
+  }, [])
+  
   return (
     <div >
-          The basic empty template
+      {
+        loading
+        ?
+        (<p>Loading</p>)
+        :
+        (<p>Got tickets</p>)
+      }
     </div>
   )
 }
