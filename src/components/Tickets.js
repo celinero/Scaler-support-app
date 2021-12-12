ce3c88d28a5ca4648adb59e969381d37c369340d
celine.roure <celine.roure.pro@gmail.com>
2021-12-12 23:14:54 +1100

    updates Tickets and ticketServices with fake backend

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React from 'react';
+import { CardContainer } from '../styled-components';
+import { Dashboard } from './Dashboard'
 
 const Tickets = (props) => {
 const {loading, tickets} = props;
 
   return(
-    <div>
-      {loading ?(<p>Loading</p>) : (<p>Got tickets</p>)}
-    </div>
+    <>
+      {loading ?
+      (<p>Loading</p>) 
+      : 
+      (<CardContainer>
+        {tickets.sort((a, b) => b.updated_at - a.updated_at).map(ticket => (<Dashboard key={ticket.id} ticket={ticket} />))}
+      </CardContainer>)
+      }
+    </>
   )
 }
 
