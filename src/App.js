bf545c29b8d3de264c6a42642eaa9d75395006f3
celine.roure <celine.roure.pro@gmail.com>
2021-12-12 23:03:15 +1100

    creates a Homepage component using Route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Tickets from './components/Tickets';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { getTickets } from './services/ticketServices';
 import { GlobalStyle } from './styled-components/globalStyles';
+import Tickets from './components/Tickets';
+import { Homepage } from './components/Homepage';
+
 
 const App = () => {
   const [tickets, setTickets] = useState([]);
@@ -23,7 +25,8 @@ const App = () => {
     <GlobalStyle />
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Tickets loading={loading} tickets={tickets} />} ></Route>
+        <Route path="/" element={<Homepage/>} />
+        <Route path="/tickets" element={<Tickets loading={loading} tickets={tickets} />}></Route>
       </Routes>
     </BrowserRouter>
     </>
