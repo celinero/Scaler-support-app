770eb3ca446a9fb1dcd739a374ab8702236296e5
celine.roure <celine.roure.pro@gmail.com>
2021-12-13 12:02:08 +1100

    starts implementing Homepage and Navbar components

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,7 +5,7 @@ import React from 'react';
 export const heroImage = () => {
   return(
     <>
-      <img src={"./images/hero-image.jpeg"}></img>
+      <img src={"./images/hero-image.jpeg"} alt="main image" ></img>
     </>
   )
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Nav, StyledLink } from '../styled-components'
+
+export const Navbar = () => {
+  return(
+    <Nav>
+      <div classname="container">
+        <div className="navbar">
+          <div className=""></div>
+        </div>
+      </div>
+    </Nav>
+  )
+}
