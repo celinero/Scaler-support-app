9bd91941fcdc63ff7465fc3ef998681a0ace68bd
celine.roure <celine.roure.pro@gmail.com>
2022-01-04 21:05:11 +1100

    add link to tickets in the navigation

--- src/components/molecules/Navbar.js
+++ src/components/molecules/Navbar.js
@@ -18,7 +18,8 @@ export const Navbar = (props) => {
 
         {user.data.isLoggedIn && <>
           {user.data.displayName} | {user.data.email}
-          <StyledLink to="/user/tickets/new">Add a post</StyledLink>
+          <StyledLink to="/user/tickets">Tickets</StyledLink>
+          <StyledLink to="/user/tickets/new">Add a ticket</StyledLink>
           <StyledLink onClick={handleLogOut} to="/">Log out</StyledLink>
         </>}
      </Nav>
