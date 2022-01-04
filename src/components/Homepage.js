2d9f6c4775c214d0c4601c12d99e9c98b45cf53f
celine.roure <celine.roure.pro@gmail.com>
2022-01-04 12:15:12 +1100

    updates Homepage and New Ticket files

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,27 @@
 import React from 'react';
+// import { useNavigate } from 'react-router';
+import { useGlobalState } from '../config/store';
+import  Tickets   from './Tickets';
+import { StyledLink } from '../styled-components'
 
+export const Homepage = () => {
+  // const navigate = useNavigate();
+  const {store, dispatch} = useGlobalState();
+  const {loggedInUser} = store;
 
-
-export const heroImage = () => {
   return(
     <>
-      <img src={"./images/hero-image.jpeg"} alt="main image" ></img>
+      <h1>Scaler Support App</h1>
+      <img src={"../images/hero-image.jpeg"} alt="" />
+
+      {loggedInUser ?
+        (<Tickets />)
+        :
+        (<StyledLink to="/login">Log in</StyledLink>)
+      }
+    
     </>
   )
+
 }
+  
