d7912de2bc8efa4d00116c47ecfd8538e809952d
celine.roure <celine.roure.pro@gmail.com>
2022-01-27 13:24:01 +1100

    change body background color

--- src/components/atoms/globalStyles.js
+++ src/components/atoms/globalStyles.js
@@ -7,7 +7,7 @@ export const GlobalStyle = createGlobalStyle`
     font-size: 14px;
     line-height: 1.4;
     color: rgb(88, 96, 105);
-    background: linear-gradient(180deg, rgba(238,237,235,1) 0%, rgba(255, 255, 255, 0) 500px) no-repeat;
+    background: linear-gradient(180deg, rgb(169 169 169) 0%, rgba(238,237,235,1) 400px) no-repeat;
     min-height: 100vh;
   } 
 
