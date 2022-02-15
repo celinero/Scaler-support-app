d4026d2f96d5d7994e1b0b7afda99b09371dc55d
celine.roure <celine.roure.pro@gmail.com>
2022-02-15 18:00:14 +1100

    update tickets with new customRender

--- src/components/pages/Tickets/Tickets.test.js
+++ src/components/pages/Tickets/Tickets.test.js
@@ -1,43 +1,18 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { BrowserRouter } from "react-router-dom";
-import { createMemoryHistory } from "history";
-import { rest } from "msw";
-import { setupServer } from "msw/node";
+import { customRender } from "utils/testing";
 import { Tickets } from ".";
 
-import { StateContext } from "config/store";
-
-const mockNavigate = jest.fn();
-
-jest.mock("react-router-dom", () => ({
-  ...jest.requireActual("react-router-dom"),
-  useNavigate: () => mockNavigate,
-}));
-
 describe("Tickets", () => {
-  let history;
-
-  const renderTickets = ({ user, tickets } = {}) => {
-    history = createMemoryHistory({ initialEntries: ["/"] });
-    render(
-      <StateContext.Provider
-        value={{
-          store: {
-            user: user || { role: "user", displayName: "Celine" },
-            tickets: tickets || [],
-          },
-        }}
-      >
-        <BrowserRouter history={history}>
-          <Tickets />
-        </BrowserRouter>
-      </StateContext.Provider>
-    );
+  const params = {
+    store: {
+      user: { role: "user", displayName: "Celine" },
+      tickets: [],
+    },
   };
 
   it("should show intro block", () => {
-    renderTickets();
+    customRender(<Tickets />, params);
 
     expect(
       screen.getByRole("heading", { name: "Hi Celine" })
@@ -48,46 +23,52 @@ describe("Tickets", () => {
   });
 
   it("should have a link to create a new ticket", () => {
-    renderTickets();
+    const { history } = customRender(<Tickets />, params);
 
-    expect(
-      screen.getByRole("link", { name: "Create a new ticket" })
-    ).toBeInTheDocument();
-    expect(
-      screen.getByRole("link", { name: "Create a new ticket" })
-    ).toHaveProperty("href", "http://localhost/user/tickets/new");
+    const createLink = screen.getByRole("link", {
+      name: "Create a new ticket",
+    });
+
+    expect(createLink).toBeInTheDocument();
+    expect(createLink).toHaveProperty(
+      "href",
+      "http://localhost/user/tickets/new"
+    );
+
+    userEvent.click(createLink);
+
+    expect(history.location.pathname).toBe("/user/tickets/new");
   });
 
   describe("when x tickets", () => {
-    beforeEach(() => {
-      renderTickets({
-        tickets: [
-          {
-            _id: "1",
-            ticketSubject: "lorem ipsum",
-            ticketResolved: false,
-            ticketSeen: true,
-            ticketMessages: [{}],
-          },
-          {
-            _id: "2",
-            ticketSubject: "dolor si amet",
-            ticketResolved: false,
-            ticketSeen: false,
-            ticketMessages: [{}],
-          },
-          {
-            _id: "3",
-            ticketSubject: "consectetur adipiscing elit",
-            ticketResolved: true,
-            ticketSeen: true,
-            ticketMessages: [{}],
-          },
-        ],
-      });
-    });
+    const newParams = JSON.parse(JSON.stringify(params));
+    newParams.store.tickets = [
+      {
+        _id: "1",
+        ticketSubject: "lorem ipsum",
+        ticketResolved: false,
+        ticketSeen: true,
+        ticketMessages: [{}],
+      },
+      {
+        _id: "2",
+        ticketSubject: "dolor si amet",
+        ticketResolved: false,
+        ticketSeen: false,
+        ticketMessages: [{}],
+      },
+      {
+        _id: "3",
+        ticketSubject: "consectetur adipiscing elit",
+        ticketResolved: true,
+        ticketSeen: true,
+        ticketMessages: [{}],
+      },
+    ];
 
     it("should show active tickets", () => {
+      customRender(<Tickets />, newParams);
+
       expect(
         screen.getByText("You have 2 active support tickets.")
       ).toBeInTheDocument();
@@ -97,6 +78,8 @@ describe("Tickets", () => {
     });
 
     it("should show solved tickets", () => {
+      customRender(<Tickets />, newParams);
+
       expect(screen.getByText("Solved tickets")).toBeInTheDocument();
       expect(
         screen.getByText("Consectetur adipiscing elit")
@@ -104,24 +87,34 @@ describe("Tickets", () => {
     });
 
     it("should show 'not seen' notification", () => {
+      customRender(<Tickets />, newParams);
+
       expect(screen.getByTestId("2--not-seen")).toBeInTheDocument();
     });
 
-    it("should go to ticket page when clicking on a ticket card", async () => {
-      expect(screen.getByRole("link", { name: /Lorem ipsum/ })).toHaveProperty(
+    it("should go to ticket page when clicking on a ticket card", () => {
+      const { history } = customRender(<Tickets />, newParams);
+
+      const linkCard = screen.getByRole("link", { name: /Lorem ipsum/ });
+
+      expect(linkCard).toBeInTheDocument();
+      expect(linkCard).toHaveProperty(
         "href",
         "http://localhost/user/tickets/1"
       );
 
-      userEvent.click(screen.getByRole("link", { name: /Lorem ipsum/ }));
+      userEvent.click(linkCard);
 
-      expect(mockNavigate).toHaveBeenCalledWith("/user/tickets/1");
+      expect(history.location.pathname).toBe("/user/tickets/1");
     });
   });
 
   describe("when user is admin", () => {
+    const newParams = JSON.parse(JSON.stringify(params));
+    newParams.store.user.role = "admin";
+
     it("should not have a link to create a new ticket", () => {
-      renderTickets({ user: { role: "admin", displayName: "Celine" } });
+      customRender(<Tickets />, newParams);
 
       expect(
         screen.queryByRole("link", { name: "Create a new ticket" })
@@ -129,18 +122,17 @@ describe("Tickets", () => {
     });
 
     it("should show 'require action' notification", () => {
-      renderTickets({
-        user: { role: "admin", displayName: "Celine" },
-        tickets: [
-          {
-            _id: "1",
-            ticketSubject: "lorem ipsum",
-            ticketResolved: false,
-            ticketSeen: true,
-            ticketMessages: [{ ticketUserRole: "user" }],
-          },
-        ],
-      });
+      newParams.store.tickets = [
+        {
+          _id: "1",
+          ticketSubject: "lorem ipsum",
+          ticketResolved: false,
+          ticketSeen: true,
+          ticketMessages: [{ ticketUserRole: "user" }],
+        },
+      ];
+
+      customRender(<Tickets />, newParams);
 
       expect(screen.getByTestId("1--require-action")).toBeInTheDocument();
     });
